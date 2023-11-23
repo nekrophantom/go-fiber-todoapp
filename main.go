92b8b349a19a0ec1e2c947153ce111ffe43dc73d
reynold <reynoldandikagradiyanto@gmail.com>
2023-11-23 14:26:08 +0700

    feat: added live reload air and misc folder

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
+	err := app.Listen("localhost:3000")
+
+	if err != nil {
+		panic(err)
+	}
+}
