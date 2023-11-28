9036bdfa2a3c8a1886a5fdeb0c20791407a31bdf
reynold <reynoldandikagradiyanto@gmail.com>
2023-11-28 13:06:41 +0700

    feat: added swagger UI

--- controller/todo_controller.go
+++ controller/todo_controller.go
@@ -10,13 +10,18 @@ type TodoController struct {
 	DB *gorm.DB
 }
 
-func NewTodoController(db *gorm.DB) *TodoController{
+func NewTodoController(db *gorm.DB) *TodoController {
 	return &TodoController{
 		DB: db,
 	}
 }
 
 // Get All
+// @Summary List Todos
+// @Description Get list of all todos
+// @Produce json
+// @Success 200 {array} models.Todo
+// @Router /todos [get]
 func (c *TodoController) GetAllTodos(ctx *fiber.Ctx) error {
 	var todos []models.Todo
 	c.DB.Find(&todos)
@@ -24,6 +29,13 @@ func (c *TodoController) GetAllTodos(ctx *fiber.Ctx) error {
 }
 
 // Get by Id
+// @Summary Show an Todo
+// @Description Get todo by ID
+// @Produce json
+// @Param id path int true "Todo ID"
+// @Success 200 {object} models.Todo
+// @Failure 404  {string}  string "Todo not found"
+// @Router /todos/{id} [get]
 func (c *TodoController) GetTodoById(ctx *fiber.Ctx) error {
 	id := ctx.Params("id")
 	var todo models.Todo
@@ -37,9 +49,16 @@ func (c *TodoController) GetTodoById(ctx *fiber.Ctx) error {
 }
 
 // Create a new Todo
+// @Summary Store Todo
+// @Description Create a Todo
+// @Produce json
+// @Param task body string true "Task Name" SchemaExample({\n"task" : "example"\n})
+// @Success 200 {object} models.Todo
+// @Failure 400  {string}  string "Invalid Request"
+// @Router /todos [post]
 func (c *TodoController) CreateTodo(ctx *fiber.Ctx) error {
 	var todo models.Todo
-	
+
 	if err := ctx.BodyParser(&todo); err != nil {
 		return ctx.Status(400).SendString("Invalid Request")
 	}
@@ -49,6 +68,15 @@ func (c *TodoController) CreateTodo(ctx *fiber.Ctx) error {
 }
 
 // Update Todo by Id
+// @Summary Update Todo
+// @Description Update an Todo By ID
+// @Produce json
+// @Param id path int true "Todo ID"
+// @Param task body string true "Task Name" SchemaExample({\n"task" : "example"\n})
+// @Success 200 {object} models.Todo
+// @Failure 400  {string}  string "Invalid Request"
+// @Failure 404  {string}  string "Todo not found"
+// @Router /todos/{id} [put]
 func (c *TodoController) UpdateTodo(ctx *fiber.Ctx) error {
 	id := ctx.Params("id")
 	var todo models.Todo
@@ -67,6 +95,13 @@ func (c *TodoController) UpdateTodo(ctx *fiber.Ctx) error {
 }
 
 // Delete Todo by Id
+// @Summary Delete Todo
+// @Description Delete an Todo By ID
+// @Produce json
+// @Param id path int true "Todo ID"
+// @Success 204 {string} string "Success Delete Todo" 
+// @Failure 404  {string}  string "Todo not found"
+// @Router /todos/{id} [delete]
 func (c *TodoController) DeleteTodo(ctx *fiber.Ctx) error {
 	id := ctx.Params("id")
 	var todo models.Todo
@@ -77,5 +112,5 @@ func (c *TodoController) DeleteTodo(ctx *fiber.Ctx) error {
 	}
 
 	c.DB.Delete(&todo)
-	return ctx.SendStatus(204)
+	return ctx.Status(200).SendString("Success Delete Todo")
 }
